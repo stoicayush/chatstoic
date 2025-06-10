76c935490fde639b172b2b9833e78130158b311f
stoicayush <stoicbanda@gmail.com>
2025-06-10 13:41:31 +0530

    fixed util.js and auth.controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://chatstoic.vercel.app"],
+    origin:  "https://chatstoic.vercel.app",
     credentials: true,
   })
 );
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+    httpOnly: true,                 // protects from XSS
+    sameSite: "None",              // allows cross-origin cookies (Vercel + Render)
+    secure: true,                  // ensures cookie is only sent over HTTPS
   });
 
   return token;
